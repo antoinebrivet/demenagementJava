8bd7116d8fb6c03fad3170e2c4a833e7e4266bcc
antoinebrivet <73895212+antoinebrivet@users.noreply.github.com>
2021-03-18 16:56:39 +0100

    Add files via upload

---
+++ demenagement.java
@@ -0,0 +1,13 @@
+package demenagement;
+
+int boxes = 34
+int boxesInTrucks = 9
+
+public class demenagement { 
+
+	for (int boxesInTrucks=9; int boxesInTrucks<=34; int++){
+	system.out.println("un voyage de" + boxesInTrucks + "camions")  
+	}
+
+	}      
+	
